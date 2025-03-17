b286e234cf2ab3bb668aed914ba3da1f81462991
agustin lavandero ivelic <agustinlavandero@gmail.com>
2025-03-16 22:37:13 -0400

    other file

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
-import { CartContext } from '../../context/CartContext'
+import { CartContext } from '../../context/cartContext'
 import './CartWidget.css'
 
 function CartWidget() {
